e9fd8692fefb4a5ebb0a5c5d0aea429bf208d43f
Max Walley <max.walley1@btinternet.com>
2021-12-14 10:55:27 +0000

    Set doxygen output to the correct place

--- Source/docs/html/search/all_1.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['testclass_2',['TestClass',['../class_test_class.html',1,'TestClass'],['../class_test_class.html#a09903432ed60c7c38f1beccc4cf18987',1,'TestClass::TestClass()']]],
-  ['testclass_2eh_3',['TestClass.h',['../_test_class_8h.html',1,'']]],
-  ['testfunc_4',['testFunc',['../class_test_class.html#a91a2a2a51bc489c7e1e97b1ed39f3a7c',1,'TestClass']]]
-];
